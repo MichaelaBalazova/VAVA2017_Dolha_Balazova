6901e51fae427c0edd864afe75578c77a46d3693
MichaelaBalazova <xbalazovam@stuba.sk>
2017-05-10 21:58:27 +0200

    pridavam uprave log prop a tiez nove featurky pre QR kody na server side

--- LibQServer/src/persistanceManagers/BooksManagerBean.java
+++ LibQServer/src/persistanceManagers/BooksManagerBean.java
@@ -16,6 +16,7 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 import entity.GenresModel;
+import entity.LocationModel;
 import entity.AuthorsModel;
 import entity.BooksModel;
 import test.BooksManagerRemote;
@@ -78,7 +79,7 @@ public class BooksManagerBean implements BooksManagerRemote {
 		return list;	
     }
     
-    public List<BooksModel> findAuthor(String name){
+    public List<BooksModel> findAuthor(String name, int limit, int offset){
     	
     	Context ctx;
     	DataSource db = null;
@@ -105,7 +106,10 @@ public class BooksManagerBean implements BooksManagerRemote {
 	        	+ "books.ean_code AS b_ean FROM books "
 	        	+ "JOIN authors_books ON authors_books.book_id = books.id "
 	        	+ "JOIN authors ON authors.id = authors_books.author_id "
-	        	+ "WHERE authors.first_name LIKE '%" + name + "%' ORDER BY books.id");
+	        	+ "WHERE (authors.first_name LIKE '%" + name + "%' "
+	        	+ "OR authors.last_name LIKE '%" + name + "%' )"
+	        	+ "ORDER BY books.id "
+	        	+ "OFFSET " + offset + " LIMIT " + limit + ";");
 	        
 	        while ( rs.next() ) {
 	        	list.add(new BooksModel(rs.getInt("b_id"), 
@@ -170,7 +174,7 @@ public class BooksManagerBean implements BooksManagerRemote {
 		return list;	
     }
     
-    public List<BooksModel> findGenre(String genre){
+    public List<BooksModel> findGenre(String genre, int limit, int offset){
     	
     	Context ctx;
     	DataSource db = null;
@@ -197,7 +201,8 @@ public class BooksManagerBean implements BooksManagerRemote {
 	        		+ "genres.add_text AS g_text "
 	        		+ "FROM books "
 	        		+ "JOIN genres ON genres.id = books.genre_id "
-	        		+ "WHERE genres.name LIKE '%" + genre + "%' ORDER BY books.id");
+	        		+ "WHERE genres.name LIKE '%" + genre + "%' ORDER BY books.id "
+	        		+ "OFFSET " + offset + " LIMIT " + limit + ";");
 	        
 	        while ( rs.next() ) {
 	        	list.add(new BooksModel(rs.getInt("b_id"), 
@@ -222,7 +227,7 @@ public class BooksManagerBean implements BooksManagerRemote {
 		return list;	
     }
     
-    public List<BooksModel> findPublicationDate(Date date){
+    public List<BooksModel> findPublicationDate(Date date, int limit, int offset){
     	
     	Context ctx;
     	DataSource db = null;
@@ -246,7 +251,8 @@ public class BooksManagerBean implements BooksManagerRemote {
 	        ResultSet rs = stm.executeQuery("SELECT books.id AS b_id, books.title AS b_title, "
 	        		+ "books.publisher AS b_publisher, books.publication_date AS b_date, "
 	        		+ "books.no_pages AS b_pgs, books.no_pieces AS b_pcs, books.ean_code AS b_ean "
-	        		+ "FROM books WHERE books.publication_date >= '" + date + "' ORDER BY books.id");
+	        		+ "FROM books WHERE books.publication_date >= '" + date + "' ORDER BY books.id "
+	        		+ "OFFSET " + offset + " LIMIT " + limit + ";");
 	        
 	        while ( rs.next() ) {
 	        	list.add(new BooksModel(rs.getInt("b_id"), 
@@ -269,6 +275,79 @@ public class BooksManagerBean implements BooksManagerRemote {
 		}
 		
 		return list;		
+	}
+    
+    public BooksModel getDetail(int book_id){
+    	
+    	Context ctx;
+    	DataSource db = null;
+    	Connection conn = null;
+  
+		try {
+			ctx = new InitialContext();
+			db = (DataSource) ctx.lookup("java:/PostgresDS");
+		} catch (NamingException e) {
+			LOG.severe("Error: "+e);
+		}
+		Statement stm = null;
+		BooksModel book = null;
+		List<AuthorsModel> list_authors = new ArrayList<AuthorsModel>();
+		
+		try{
+			conn = db.getConnection();
+			if (conn == null) {
+				LOG.severe("Failed to make connection in BooksManagerBean.getAllBooks!");
+			}
+			
+			stm = conn.createStatement();
+	        ResultSet rs = stm.executeQuery("SELECT books.id AS b_id, books.title AS b_title, genres.name AS g_name, " + 
+	        	"books.publisher AS b_publisher, authors.first_name AS a_first, authors.last_name AS a_last, " +
+	        	"books.publication_date AS b_date, books.no_pages AS b_pgs, books.no_pieces AS b_pcs, books.ean_code AS b_ean, " +
+	        	"genres.add_text AS g_text, location.floor AS l_floor, location.room AS l_room, location.row AS l_row, " +
+	        	"location.shelf AS l_shelf, authors.add_text AS a_text " +
+	        	"FROM books " +
+				"JOIN genres ON genres.id = books.genre_id " + 
+				"JOIN location ON location.id = books.location_id " + 
+				"JOIN authors_books ON authors_books.book_id = books.id " + 
+				"JOIN authors ON authors.id = authors_books.author_id " + 
+				"WHERE books.id = " + book_id +
+				" ORDER BY books.id;");
+	        
+	        if ( rs.next() ){
+	        		book = new BooksModel(rs.getInt("b_id"),
+	        					rs.getString("b_title"), 
+	        					rs.getString("b_publisher"), 
+	        					rs.getDate("b_date"),
+	        					rs.getInt("b_pgs"), 
+	        					rs.getInt("b_pcs"), 
+	        					rs.getString("b_ean"), 
+	        					
+	        					new GenresModel(rs.getString("g_name"), rs.getString("g_text")),
+	        					
+	        					new LocationModel(    					
+	        					rs.getInt("l_floor"),
+	        		 	        rs.getInt("l_room"),
+	        		 	        rs.getInt("l_row"), 
+	        		 	        rs.getInt("l_shelf"))
+	        				        		 	        	        					
+	        		);
+	        		list_authors.add(new AuthorsModel(rs.getString("a_first"), rs.getString("a_last"), rs.getString("a_text")));
+	        		while ( rs.next() ){
+	        			list_authors.add(new AuthorsModel(rs.getString("a_first"), rs.getString("a_last"), rs.getString("a_text")));	        			
+	        		}	
+	        		book.setAuthors_list(list_authors);
+	        }
+	        rs.close();
+		}
+		catch (SQLException e1) {
+			LOG.severe("Error: "+e1);
+		}
+		finally {
+			try { if (stm != null) stm.close(); } catch (Exception e) {};
+		    try { if (conn != null) conn.close(); } catch (Exception e) {};
+		}
+		
+		return book;		
 	}
     
 }
